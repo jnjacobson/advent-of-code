c9d75846212ca15fa15009f848fe2df1714b9fc3
jannj <jannjacobson1997@gmail.com>
2022-12-03 14:52:31 +0100

    refactoring

--- src/2022/3/solution.ts
+++ src/2022/3/solution.ts
@@ -2,7 +2,7 @@ import { readFileSync } from 'fs';
 
 const input = readFileSync('src/2022/3/input.txt', 'utf-8');
 
-const lines = input
+const rucksacks = input
   .split('\n')
   .filter((line: string) => line.length > 0);
 
@@ -14,11 +14,11 @@ const getItemPriority = (item: string): number => {
   return item.charCodeAt(0) - asciiToPriority;
 }
 
-const solutionPart1 = lines
-  // split into the 2 compartments
-  .map((line: string): [string, string] => [
-    line.slice(0, line.length / 2),
-    line.slice(line.length / 2)
+const solutionPart1 = rucksacks
+  // split rucksack into the 2 compartments
+  .map((rucksack: string): [string, string] => [
+    rucksack.slice(0, rucksack.length / 2),
+    rucksack.slice(rucksack.length / 2)
   ])
   // get priority of common item
   .map(([compartment1, compartment2]): number => {
@@ -32,25 +32,28 @@ const solutionPart1 = lines
 
     return getItemPriority(item);
   })
+  // sum priorities
   .reduce((acc, val) => acc + val, 0);
 
 
-const solutionPart2 = lines
+const solutionPart2 = rucksacks
   // get groups of three
-  .reduce((groups, val): string[][] => {
+  .reduce((groups: string[][], rucksack: string): string[][] => {
     if (groups[groups.length - 1].length === 3) {
       groups.push([]); // last group is full, make new one
     }
 
-    groups[groups.length - 1].push(val);
+    groups[groups.length - 1].push(rucksack);
 
     return groups;
-  }, [[]] as string[][])
+  }, [])
   // get priority of common item
-  .map(([inv1, inv2, inv3]) => {
-    const item = inv1
+  .map(([rucksack1, rucksack2, rucksack3]) => {
+    const item = rucksack1
       .split('')
-      .find((item: string) => inv2.includes(item) && inv3.includes(item));
+      .find((item: string) => (
+        rucksack2.includes(item) && rucksack3.includes(item)
+      ));
 
     if (item === undefined) {
       throw new Error('No common item found!');
@@ -58,7 +61,7 @@ const solutionPart2 = lines
 
     return getItemPriority(item);
   })
-  // sum
+  // sum priorities
   .reduce((acc, val) => acc + val, 0);
 
 console.log({
