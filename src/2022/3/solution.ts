c31d78a4459db9f2fecf11c02fc062c448e317c5
jannj <jannjacobson1997@gmail.com>
2022-12-03 14:39:28 +0100

    day 3 part 2

--- src/2022/3/solution.ts
+++ src/2022/3/solution.ts
@@ -6,6 +6,14 @@ const lines = input
   .split('\n')
   .filter((line: string) => line.length > 0);
 
+const getItemPriority = (item: string): number => {
+  const asciiToPriority = item.toLowerCase() === item
+    ? 97 - 1
+    : 65 - 27;
+
+  return item.charCodeAt(0) - asciiToPriority;
+}
+
 const solutionPart1 = lines
   .map((line: string): [string, string] => [
     line.slice(0, line.length / 2),
@@ -20,14 +28,35 @@ const solutionPart1 = lines
       throw new Error('No common item found!');
     }
 
-    const asciiToPriority = item.toLowerCase() === item
-      ? 97 - 1
-      : 65 - 27;
+    return getItemPriority(item);
+  })
+  .reduce((acc, val) => acc + val, 0);
 
-    return item.charCodeAt(0) - asciiToPriority;
+
+const solutionPart2 = lines
+  .reduce((triples, val): string[][] => {
+    if (triples[triples.length - 1].length === 3) {
+      triples.push([]); // last triple is full, make new one
+    }
+
+    triples[triples.length - 1].push(val);
+
+    return triples;
+  }, [[]] as string[][])
+  .map(([inv1, inv2, inv3]) => {
+    const item = inv1
+      .split('')
+      .find((item: string) => inv2.includes(item) && inv3.includes(item));
+
+    if (item === undefined) {
+      throw new Error('No common item found!');
+    }
+
+    return getItemPriority(item);
   })
   .reduce((acc, val) => acc + val, 0);
 
 console.log({
   solutionPart1,
+  solutionPart2,
 })
