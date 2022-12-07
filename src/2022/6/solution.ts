01a67ecdfe43735efb4f9b04ccca09bab71cef24
jannj <jannjacobson1997@gmail.com>
2022-12-07 21:07:33 +0100

    refactor

--- src/2022/6/solution.ts
+++ src/2022/6/solution.ts
@@ -3,16 +3,16 @@ import { readFileSync } from 'fs';
 const inputStr = readFileSync('src/2022/6/input.txt', 'utf-8');
 
 const findMarkerPos = (str: string, markerLen: number): number => {
-  for (let i = markerLen - 1; i < str.length; i++) {
+  for (let i = markerLen; i < str.length; i++) {
     const isMarker = str
-      .slice(i - markerLen + 1, i + 1)
+      .slice(i - markerLen, i)
       .split('')
       .every((char, j, possibleMarker) => (
         !possibleMarker.includes(char, j + 1)
       ));
 
     if (isMarker) {
-      return i + 1;
+      return i;
     }
   }
 
