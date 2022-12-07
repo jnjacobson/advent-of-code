14cf075a381d2ed7b632836d493f4653d6c8a8ed
jannj <jannjacobson1997@gmail.com>
2022-12-07 21:02:15 +0100

    day 6

---
+++ src/2022/6/solution.ts
@@ -0,0 +1,25 @@
+import { readFileSync } from 'fs';
+
+const inputStr = readFileSync('src/2022/6/input.txt', 'utf-8');
+
+const findMarkerPos = (str: string, markerLen: number): number => {
+  for (let i = markerLen - 1; i < str.length; i++) {
+    const isMarker = str
+      .slice(i - markerLen + 1, i + 1)// get last 4 chars
+      .split('')
+      .every((char, j, possibleMarker) => (
+        !possibleMarker.includes(char, j + 1)
+      ));
+
+    if (isMarker) {
+      return i + 1;
+    }
+  }
+
+  throw Error('No marker found!');
+}
+
+console.log({
+  solutionPart1: findMarkerPos(inputStr, 4),
+  solutionPart2: findMarkerPos(inputStr, 14),
+})
