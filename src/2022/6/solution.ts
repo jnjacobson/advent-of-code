651fe6e2ba3dd7bd9701548ff0cc2c066e3628ec
jannj <jannjacobson1997@gmail.com>
2022-12-07 21:03:19 +0100

    remove comment

--- src/2022/6/solution.ts
+++ src/2022/6/solution.ts
@@ -5,7 +5,7 @@ const inputStr = readFileSync('src/2022/6/input.txt', 'utf-8');
 const findMarkerPos = (str: string, markerLen: number): number => {
   for (let i = markerLen - 1; i < str.length; i++) {
     const isMarker = str
-      .slice(i - markerLen + 1, i + 1)// get last 4 chars
+      .slice(i - markerLen + 1, i + 1)
       .split('')
       .every((char, j, possibleMarker) => (
         !possibleMarker.includes(char, j + 1)
