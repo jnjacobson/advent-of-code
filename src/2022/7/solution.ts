49e97c9be99ca89d58643c6983b6a778ffa82e88
jannj <jannjacobson1997@gmail.com>
2023-02-04 14:50:09 +0100

    use 2 spaces

--- src/2022/7/solution.ts
+++ src/2022/7/solution.ts
@@ -1,63 +1,63 @@
 import { readFileSync } from 'fs';
 
 const terminalOutput = readFileSync('src/2022/7/input.txt', 'utf-8')
-    .split('\n');
+  .split('\n');
 
 const location: string[] = [];
 
 const getPath = (loc: string[]) => '/'.concat(loc.join('/'));
 
 const getSubDirPaths = (loc: string[]) => [
-    '/',
-    ...loc.map((_, idx) => getPath(loc.slice(0, idx + 1)))
+  '/',
+  ...loc.map((_, idx) => getPath(loc.slice(0, idx + 1)))
 ];
 
 const dirs: Record<string, number> = {
-    '/': 0,
+  '/': 0,
 };
 
 terminalOutput.forEach((line: string) => {
-    const parts = line.split(' ');
+  const parts = line.split(' ');
 
-    if (parts[0] === '$') {
-        if (parts[1] === 'ls') {
-            return; // ignore ls command
-        }
-
-        switch (parts[2]) {
-            case '..':
-                location.pop();
-                return;
-            case '/':
-                location.length = 0;
-                return;
-            default:
-                location.push(parts[2]);
-                return;
-        }
+  if (parts[0] === '$') {
+    if (parts[1] === 'ls') {
+      return; // ignore ls command
     }
 
-    if (parts[0] === 'dir') {
-        const path = getPath([...location, parts[1]]);
-
-        if (dirs[path] === undefined) {
-            dirs[path] = 0; // init dir
-        }
-
+    switch (parts[2]) {
+      case '..':
+        location.pop();
+        return;
+      case '/':
+        location.length = 0;
+        return;
+      default:
+        location.push(parts[2]);
         return;
     }
+  }
 
-    getSubDirPaths(location).forEach((path) => {
-        dirs[path] += Number.parseInt(parts[0]);
-    })
+  if (parts[0] === 'dir') {
+    const path = getPath([...location, parts[1]]);
+
+    if (dirs[path] === undefined) {
+      dirs[path] = 0; // init dir
+    }
+
+    return;
+  }
+
+  getSubDirPaths(location).forEach((path) => {
+    dirs[path] += Number.parseInt(parts[0]);
+  })
 });
 
 const MAX_DIR_SIZE = 100_000;
 
 const answerPart1 = Object.values(dirs)
-    .reduce((totalSize, dirSize) => (
-        dirSize > MAX_DIR_SIZE ? totalSize : totalSize + dirSize
-    ), 0);
+  .reduce((totalSize, dirSize) => (
+    dirSize > MAX_DIR_SIZE ? totalSize : totalSize + dirSize
+  ), 0);
 
 const TOTAL_DISK_SPACE = 70_000_000;
 const NEEDED_DISK_SPACE = 30_000_000;
@@ -69,12 +69,12 @@ const minDirSizeToBeDeleted = NEEDED_DISK_SPACE - (TOTAL_DISK_SPACE - usedDiskSp
 let answerPart2 = usedDiskSpace;
 
 Object.values(dirs).forEach((dirSize) => {
-    if (dirSize >= minDirSizeToBeDeleted && dirSize < answerPart2) {
-        answerPart2 = dirSize;
-    }
+  if (dirSize >= minDirSizeToBeDeleted && dirSize < answerPart2) {
+    answerPart2 = dirSize;
+  }
 });
 
 console.log({
-    answerPart1,
-    answerPart2,
+  answerPart1,
+  answerPart2,
 });
