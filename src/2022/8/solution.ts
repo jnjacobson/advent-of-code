24c1578fe864702544fd34ab793bb88e1975b678
jannj <jannjacobson1997@gmail.com>
2023-02-04 15:40:10 +0100

    add day 8 solution part 2, some renamings

--- src/2022/8/solution.ts
+++ src/2022/8/solution.ts
@@ -9,59 +9,86 @@ const trees: number[][] = readFileSync('src/2022/8/input.txt', 'utf-8')
 
 const visibleTrees: Set<string> = new Set(); // "row,col"
 
-const maxTreeHeightPerCol: number[] = Array(trees[0].length).fill(-1);
+const highestTreePerCol: number[] = Array(trees[0].length).fill(-1);
 
-trees.forEach((line, row) => {
+trees.forEach((row, rowIdx) => {
   let maxTreeHeightInLine = -1;
 
-  line.forEach((treeHeight, col) => {
+  row.forEach((tree, colIdx) => {
     let isVisible = false;
 
-    if (treeHeight > maxTreeHeightInLine) {
-      maxTreeHeightInLine = treeHeight;
+    if (tree > maxTreeHeightInLine) {
+      maxTreeHeightInLine = tree;
       isVisible = true;
     }
 
-    if (treeHeight > maxTreeHeightPerCol[col]) {
-      maxTreeHeightPerCol[col] = treeHeight;
+    if (tree > highestTreePerCol[colIdx]) {
+      highestTreePerCol[colIdx] = tree;
       isVisible = true;
     }
 
     if (isVisible) {
-      visibleTrees.add([row, col].toString());
+      visibleTrees.add([rowIdx, colIdx].toString());
     }
   });
 });
 
-maxTreeHeightPerCol.fill(-1); // reset
+highestTreePerCol.fill(-1); // reset
 
-[...trees].reverse().forEach((line, row) => {
-  const reversedRow = (trees.length - 1) - row;
-  let maxTreeHeightInLine = -1;
+[...trees].reverse().forEach((row, rowIdx) => {
+  const reversedRowIdx = (trees.length - 1) - rowIdx;
+  let highestTreeInRow = -1;
 
-  [...line].reverse().forEach((treeHeight, col) => {
-    const reversedCol = (line.length - 1) - col;
+  [...row].reverse().forEach((tree, colIdx) => {
+    const reversedColIdx = (row.length - 1) - colIdx;
     let isVisible = false;
 
-    if (treeHeight > maxTreeHeightInLine) {
-      maxTreeHeightInLine = treeHeight;
+    if (tree > highestTreeInRow) {
+      highestTreeInRow = tree;
       isVisible = true;
     }
 
-    if (treeHeight > maxTreeHeightPerCol[reversedCol]) {
-      maxTreeHeightPerCol[reversedCol] = treeHeight;
+    if (tree > highestTreePerCol[reversedColIdx]) {
+      highestTreePerCol[reversedColIdx] = tree;
       isVisible = true;
     }
 
     if (isVisible) {
-      visibleTrees.add([reversedRow, reversedCol].toString());
+      visibleTrees.add([reversedRowIdx, reversedColIdx].toString());
     }
   });
 });
 
 let maxScenicScore = 0;
 
-// todo: part 2
+const getColFromMatrix = <T>(arr: T[][], col: number): T[] => (
+  arr.map((x) => x[col])
+);
+
+const getViewingDistance = (tree: number, otherTrees: number[]): number => {
+  if (otherTrees.length === 0) {
+    return 0;
+  }
+
+  const idx = otherTrees.findIndex((otherTree) => otherTree >= tree);
+
+  return idx === -1 ? otherTrees.length : idx + 1;
+}
+
+trees.forEach((row, rowIdx) => (
+  row.forEach((tree, colIdx) => {
+    const col = getColFromMatrix(trees, colIdx);
+
+    const scenicScore = getViewingDistance(tree, row.slice(0, colIdx).reverse()) // left
+      * getViewingDistance(tree, row.slice(colIdx + 1)) // right
+      * getViewingDistance(tree, col.slice(0, rowIdx).reverse()) // up
+      * getViewingDistance(tree, col.slice(rowIdx + 1)); // down
+
+    if (scenicScore > maxScenicScore) {
+      maxScenicScore = scenicScore;
+    }
+  })
+));
 
 console.log({
   answerPart1: visibleTrees.size,
